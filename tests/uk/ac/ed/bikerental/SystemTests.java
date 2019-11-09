c3fa976c92aba6641af81050129835a2859a77b4
Harry Lennox <harryjlennox@live.co.uk>
2019-11-09 15:06:34 +0000

    Branch init. Inserted some test experimentation.

--- tests/uk/ac/ed/bikerental/SystemTests.java
+++ tests/uk/ac/ed/bikerental/SystemTests.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -22,6 +23,10 @@ public class SystemTests {
         DeliveryServiceFactory.setupMockDeliveryService();
         
         // Put your test setup here
+        BikeProvider prov1 = new BikeProvider();
+        BikeProvider prov2 = new BikeProvider();
+        BikeProvider prov3 = new BikeProvider();
+        QuoteController controller = new QuoteController();
     }
     
     // TODO: Write system tests covering the three main use cases
@@ -30,5 +35,13 @@ public class SystemTests {
     void myFirstTest() {
         // JUnit tests look like this
         assertEquals("The moon", "cheese"); // Should fail
+    }
+    
+    // Test the happy scenario - for a correct input, nearby correct quotes should be shown.
+    // Query should return quotes from the two nearby stores (prov1 and prov2) but not prov3.
+    @Test
+    void returnQuotesForCorrectInput() {
+    	Collection<Quote> quotes = controller.getQuotes();
+    	assetEquals(quotes.length, 2);
     }
 }
