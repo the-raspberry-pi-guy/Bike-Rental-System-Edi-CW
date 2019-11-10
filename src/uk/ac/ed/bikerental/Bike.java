00e773758febd81423ddf2465d5c67dd843aae79
Matthew Timmons-Brown <matt@Matthews-MacBook-Pro.local>
2019-11-10 14:28:43 +0000

    Populate Bike class with date, bike type and constructor to BikeType class

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -1,9 +1,19 @@
 package uk.ac.ed.bikerental;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Bike {
-    public BikeType getType() {
-        // TODO: Implement Bike.getType
-        assert false;
-        return null;
+    public LocalDate dateOfPurchase;
+	public BikeType bikeType;
+    
+    // Constructor to set attributes
+    public Bike(String type, BigDecimal replacementValue, LocalDate date) {
+        dateOfPurchase = date;
+        bikeType = new BikeType(type, replacementValue);
+    }
+	
+    public String getType() {
+        return bikeType.getBikeType();
     }
 }
